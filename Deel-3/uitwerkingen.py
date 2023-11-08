d65a5857deb42d4f5b9aee8a7684b5984e178460
Djurre <djurre10@hotmail.com>
2023-11-08 15:01:22 +0100

    init

---
+++ Deel-3/uitwerkingen.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+# OPGAVE 1a
+def plot_image(img, label):
+    # Deze methode krijgt een matrix mee (in img) en een label dat correspondeert met het 
+    # plaatje dat in de matrix is weergegeven. Zorg ervoor dat dit grafisch wordt weergegeven.
+    # Maak gebruik van plt.cm.binary voor de cmap-parameter van plt.imgshow.
+
+    # YOUR CODE HERE
+    plt.figure()  # maak een nieuw figuur
+    plt.imshow(img, cmap=plt.cm.binary)  # toon de afbeelding
+    plt.xlabel(label)  # zet het label op de x-as
+    plt.show()
+
+    pass
+
+
+# OPGAVE 1b
+def scale_data(X):
+    # Deze methode krijgt een matrix mee waarin getallen zijn opgeslagen van 0..m, en hij 
+    # moet dezelfde matrix retourneren met waarden van 0..1. Deze methode moet werken voor 
+    # alle maximale waarde die in de matrix voorkomt.
+    # Deel alle elementen in de matrix 'element wise' door de grootste waarde in deze matrix.
+
+    # YOUR CODE HERE
+    max_value = np.amax(X)  # bepaal de maximale waarde in de matrix
+    scaled_X = X / max_value  # deel alle elementen in de matrix door de maximale waarde
+
+    return scaled_X
+
+
+# OPGAVE 1c
+def build_model():
+    # Deze methode maakt het keras-model dat we gebruiken voor de classificatie van de mnist
+    # dataset. Je hoeft deze niet abstract te maken, dus je kunt er van uitgaan dat de input
+    # layer van dit netwerk alleen geschikt is voor de plaatjes in de opgave (wat is de 
+    # dimensionaliteit hiervan?).
+    # Maak een model met een input-laag, een volledig verbonden verborgen laag en een softmax
+    # output-laag. Compileer het netwerk vervolgens met de gegevens die in opgave gegeven zijn
+    # en retourneer het resultaat.
+
+    # Het staat je natuurlijk vrij om met andere settings en architecturen te experimenteren.
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(10, activation='softmax')  # output layer
+    ])
+
+    # YOUR CODE HERE
+    model.compile(optimizer='adam',  # optimizer
+                  loss='sparse_categorical_crossentropy',  # loss function
+                  metrics=['accuracy'])  # metrics
+
+    return model
+
+
+# OPGAVE 2a
+def conf_matrix(labels, pred):
+    # Retourneer de econfusion matrix op basis van de gegeven voorspelling (pred) en de actuele
+    # waarden (labels). Check de documentatie van tf.math.confusion_matrix:
+    # https://www.tensorflow.org/api_docs/python/tf/math/confusion_matrix
+
+    # YOUR CODE HERE
+    # confusion matrix
+    conf_mtx = tf.math.confusion_matrix(labels, pred)  # confusion matrix
+    # return confusion matrix
+    return conf_mtx
+
+
+# OPGAVE 2b
+def conf_els(conf, labels):
+    # Deze methode krijgt een confusion matrix mee (conf) en een set van labels. Als het goed is, is 
+    # de dimensionaliteit van de matrix gelijk aan len(labels) × len(labels) (waarom?). Bereken de 
+    # waarden van de TP, FP, FN en TN conform de berekening in de opgave. Maak vervolgens gebruik van
+    # de methodes zip() en list() om een list van len(labels) te retourneren, waarbij elke tupel 
+    # als volgt is gedefinieerd:
+
+    #     (categorie:string, tp:int, fp:int, fn:int, tn:int)
+
+    # Check de documentatie van numpy diagonal om de eerste waarde te bepalen.
+    # https://numpy.org/doc/stable/reference/generated/numpy.diagonal.html
+
+    # YOUR CODE HERE
+    # TP
+    tp = np.diagonal(conf)  # true positive
+    # FP
+    fp = np.sum(conf, axis=0) - tp  # false positive
+    # FN
+    fn = np.sum(conf, axis=1) - tp  # false negative
+    # TN
+    tn = np.sum(conf) - tp - fp - fn  # true negative
+
+    # return list of tuples
+    return list(zip(labels, tp, fp, fn, tn))  # list of tuples
+
+
+# OPGAVE 2c
+def conf_data(metrics):
+    # Deze methode krijgt de lijst mee die je in de vorige opgave hebt gemaakt (dus met lengte len(labels))
+    # Maak gebruik van een list-comprehension om de totale tp, fp, fn, en tn te berekenen en 
+    # bepaal vervolgens de metrieken die in de opgave genoemd zijn. Retourneer deze waarden in de
+    # vorm van een dictionary (de scaffold hiervan is gegeven).
+
+    tp = sum([x[1] for x in metrics])  # true positive
+    fp = sum([x[2] for x in metrics])  # false positive
+    fn = sum([x[3] for x in metrics])  # false negative
+    tn = sum([x[4] for x in metrics])  # true negative
+
+    # BEREKEN HIERONDER DE JUISTE METRIEKEN EN RETOURNEER DIE 
+    # ALS EEN DICTIONARY
+
+    # YOUR CODE HERE
+    # tpr
+    tpr = tp / (tp + fn)  # true positive rate
+    # ppv
+    ppv = tp / (tp + fp)  # positive predictive value
+    # tnr
+    tnr = tn / (tn + fp)  # true negative rate
+    # fpr
+    fpr = fp / (fp + tn)  # false positive rate
+
+    # return dictionary
+    rv = {'tpr': tpr, 'ppv': ppv, 'tnr': tnr, 'fpr': fpr}
+    return rv
